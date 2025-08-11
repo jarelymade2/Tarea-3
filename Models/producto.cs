766f30a6a5f3595b3909114de55fe1a6482a5e2c
Jarely M. <jarely643@gmail.com>
2025-08-10 21:31:07 -0500

    model de productos

---
+++ Models/producto.cs
@@ -0,0 +1,9 @@
+namespace Tarea_3.Models
+{
+    public class producto
+    {
+        public string nom { get; set; }
+        public string tipo { get; set; }
+        public decimal prec { get; set; }
+    }
+}
